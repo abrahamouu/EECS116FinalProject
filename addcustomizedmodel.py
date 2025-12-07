ec498d4da51c240776d1d05ba9520d3f9b0b35d1
mschinazi1 <mschinazi1@gmail.com>
2025-12-06 16:56:15 -0800

    Update addcustomizedmodel.py, passes autograder

--- addcustomizedmodel.py
+++ addcustomizedmodel.py
@@ -1,2 +1,19 @@
 def addCustomizedModel(mydb, *args):
-    return
+    #INSERT INTO CustomizedModel (bmid, mid)
+    #VALUES (args[1], args[0])
+    cursor = mydb.cursor()
+
+    #get bmid and mid from command line args
+    ids = args[1], args[0]
+
+    #sql insert into statement
+    sql = "INSERT INTO CustomizedModel (bmid, mid) VALUES (%s,%s)"
+    
+    #try to insert new CustomizedModel and print Success if able 
+    try:
+        cursor.execute(sql, ids)
+        mydb.commit()
+        print("Success")
+    except:
+        print("Fail")
+    return 
