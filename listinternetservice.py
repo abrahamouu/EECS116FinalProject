56ae0b4c2f4173d84e256704345de99c025b9d76
mschinazi1 <mschinazi1@gmail.com>
2025-12-08 21:20:03 -0800

    Update listinternetservice.py, passed autograder

--- listinternetservice.py
+++ listinternetservice.py
@@ -1,2 +1,24 @@
 def listInternetService(mydb, *args):
+    #SELECT * FROM InternetService WHERE sid = __ ORDER BY provider ASC
+
+    bmid = (args[0],) #get bmid from user input
+    cursor = mydb.cursor()
+
+    try: #list services if no error
+        
+        #for each sid, list internet services data using a join from modelservices
+        list_IS = "SELECT i.sid, i.endpoints, i.provider FROM InternetService i JOIN ModelServices M ON M.sid = i.sid WHERE M.bmid = %s ORDER BY i.provider ASC;"
+        cursor.execute(list_IS, bmid)
+        internet_services = cursor.fetchall()
+
+        #print in csv format
+        for element in internet_services:
+            print( "%s,%s,%s"% element)
+        
+        #if no results at all, fails
+        if(len(internet_services) == 0):
+            print("Fail")
+    except:
+       print("Fail")
+   
     return
